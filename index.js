da877ddae8d9c64a1dfb19c3f82d90b12e85c735
Ariel Arzamendia <arzamendiaariel@gmail.com>
2023-01-28 15:17:47 -0300

    regex

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const cheerio = require('cheerio');
 const request = require('request-promise');
 const fs = require('fs')
+require("dotenv").config()
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-// https://www.infodolar.com/cotizacion-dolar-localidad-rosario-provincia-santa-fe.aspx
 
 async function init(){
 
@@ -10,23 +12,37 @@ async function init(){
         uri: 'https://www.infodolar.com/cotizacion-dolar-localidad-rosario-provincia-santa-fe.aspx',
         transform: body => cheerio.load(body)
     })
-    // const websiteTitle = $('title').text().trim()
-    // console.log(websiteTitle)
-    // const titulos= $('.superTitulo').text().trim()
-    // console.log(titulos)
-    // const Blue = $('#BluePromedio').find('.colCompraVenta').html()
-    // console.log(Blue)
-    const compraBlue = $('#BluePromedio > tbody > tr > td:nth-child(2)').text().replaceAll("  ", "").replaceAll("\n", "").split("$")[1].trim()
-    const ventaBlue = $('#BluePromedio > tbody > tr > td:nth-child(3)').text().replaceAll("  ", "").replaceAll("\n", "").split("$")[1].trim()
-    
+
+    const compraBlue = $('#BluePromedio > tbody > tr > td:nth-child(2)').text().replace(/(\r\n|\n|\r|\s)/gm, "").split("$")[1].trim()
+    const ventaBlue = $('#BluePromedio > tbody > tr > td:nth-child(3)').text().replace(/(\r\n|\n|\r|\s)/gm, "").split("$")[1].trim()
+
     const cotizacion= `Blue comprador: $ ${compraBlue}
     Blue vendedor: $ ${ventaBlue}`
+
+    const msg = {
+        to: ['someguy@gmail.com', 'estudio@gmail.com'],
+        from: 'arzamendiaariel@gmail.com',
+        subject: 'Cotización dolar blue',
+        text: cotizacion,
+        html: `<strong>${cotizacion}</strong>`,
+      };
+
+      await sgMail.send(msg)
+          .then(() => console.log('Email sent successfully'))
+          .catch(error => console.error(error.toString()));
 
     fs.writeFile("cotizacion.txt", cotizacion, "utf8", (err) => {
         if (err) throw err;
         console.log('The file has been saved!')})
 
-
 }
 
+
 init()
+
+var now = new Date();
+var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 13, 6, 0, 0) - now;
+if (millisTill10 < 0) {
+     millisTill10 += 86400000; // it's after 10am, try 10am tomorrow.
+}
+setTimeout(function(){init()}, millisTill10);
